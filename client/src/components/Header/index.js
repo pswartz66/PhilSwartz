f491e017581c82adecdad127b86d324a848f1d0b
Phil Swartz <pswartz.sju@gmail.com>
2020-03-11 21:12:51 -0400

    added navbar

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+import Nav from '../Nav'
+
+const Header = () => {
+
+    return (
+        <div>
+            <header id="home">
+                <Nav />
+            </header>
+        </div>
+    )
+
+}
+
+export default Header;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+const Nav = () => {
+
+    return (
+        <div>
+            <ul id="nav">
+                <ul>Home</ul>
+                <ul>About</ul>
+                <ul>Work</ul> 
+                <ul>Contact</ul> 
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
