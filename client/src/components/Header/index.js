33714657ef735b251c3f72f5d5451c97042654dd
Phil Swartz <pswartz.sju@gmail.com>
2020-03-13 23:26:34 -0400

    modified home screen, greened intro text and added divider, fixed navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import './style.css';
-import Nav from '../Nav'
+import CustomNav from '../CustomNav'
 
 const Header = () => {
 
     return (
         <div>
             <header id="home">
-                <Nav />
-                <section id="intro">
-                    Hi, I'm Phil.
+                <CustomNav />
+                <section id="intro-container">
+                    <div id="intro">
+
+                        Hi, I'm Phil. <span className="span-text">I design and build websites in React.
+                        </span>
+                        <div id="sub-intro">
+                            Follow me @
+                        </div>
+                        {/* <div className="social-icons">
+                            fiuwheifuhwi
+                        </div> */}
+                        <hr className="divider"></hr>
+                    </div>
+
                 </section>
+
+
             </header>
 
         </div>
