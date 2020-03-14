3af392f076c547a4c369328d7d0b947c56d1b21c
Phil Swartz <pswartz.sju@gmail.com>
2020-03-14 10:49:16 -0400

    updated header section

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import './style.css';
-import CustomNav from '../CustomNav'
+import CustomNav from '../CustomNav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import styled, { keyframes } from 'styled-components';
+import slideInLeft from 'react-animations/lib/slide-in-left';
+import slideInRight from 'react-animations/lib/slide-in-right';
+import fadeOutUpBig from 'react-animations/lib/fade-in-up-big';
+import { FaGithub, FaLinkedinIn, FaInstagram, FaAngleDown } from 'react-icons/fa';
+
+
+const leftSlide = keyframes`${slideInLeft}`;
+const SlideLeft = styled.div`
+  animation: 2.9s ${leftSlide};
+`;
+const rightSlide = keyframes`${slideInRight}`;
+const SlideRight = styled.div`
+  animation: 2.9s ${rightSlide};
+`;
+const myFadeBigGit = keyframes`${fadeOutUpBig}`;
+const FadeBigGit = styled.div`
+  animation: 1.1s ${myFadeBigGit};
+`;
+const myFadeBigLink = keyframes`${fadeOutUpBig}`;
+const FadeBigLink = styled.div`
+  animation: 1.3s ${myFadeBigLink};
+`;
+const myFadeBigGram = keyframes`${fadeOutUpBig}`;
+const FadeBigGram = styled.div`
+  animation: 1.6s ${myFadeBigGram};
+`;
 
 const Header = () => {
 
@@ -11,25 +40,58 @@ const Header = () => {
                 <section id="intro-container">
                     <div id="intro">
 
-                        Hi, I'm Phil. <span className="span-text">I design and build websites in React.
+                        <SlideLeft>Hi, I'm Phil. <span className="span-text">I design and build websites in React.
                         </span>
-                        <div id="sub-intro">
-                            Follow me @
-                        </div>
-                        {/* <div className="social-icons">
-                            fiuwheifuhwi
-                        </div> */}
-                        <hr className="divider"></hr>
+                            <div className="divider-two"></div>
+                        </SlideLeft>
+
+                        <SlideRight>
+
+                            <Row>
+                                <Col></Col>
+                                <Col></Col>
+
+                                <Col>
+                                    <FadeBigGit>
+                                        <FaGithub size={24} />
+                                    </FadeBigGit>
+
+                                </Col>
+                                <Col>
+                                    <FadeBigLink>
+                                        <FaLinkedinIn size={24} />
+                                    </FadeBigLink>
+                                </Col>
+                                <Col>
+                                    <FadeBigGram>
+                                        <FaInstagram size={24} />
+                                    </FadeBigGram>
+                                </Col>
+                                <Col></Col>
+                                <Col></Col>
+
+                            </Row>
+                        </SlideRight>
+                        <SlideLeft>
+                            <Row>
+
+                                <Col>
+                                    <hr className="divider"></hr>
+                                </Col>
+
+                            </Row>
+                        </SlideLeft>
+                        <Row>
+                            <Col>
+                                <FaAngleDown color={'rgb(94, 248, 114)'} size={28} />
+                            </Col>
+                        </Row>
                     </div>
 
                 </section>
-
-
             </header>
-
         </div>
     )
-
 }
 
 export default Header;
