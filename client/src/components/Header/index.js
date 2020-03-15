a79d27ffce9f61f9b96126daa1099e08eb0694b4
Phil Swartz <pswartz.sju@gmail.com>
2020-03-14 21:40:29 -0400

    added links to social media

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -7,9 +7,11 @@ import styled, { keyframes } from 'styled-components';
 import slideInLeft from 'react-animations/lib/slide-in-left';
 import slideInRight from 'react-animations/lib/slide-in-right';
 import fadeOutUpBig from 'react-animations/lib/fade-in-up-big';
+import fadeInUp from 'react-animations/lib/fade-in-up';
 import { FaGithub, FaLinkedinIn, FaInstagram, FaAngleDown } from 'react-icons/fa';
+// import { Link } from 'react-router-dom';
 
-
+// Animations for header section on initial page load
 const leftSlide = keyframes`${slideInLeft}`;
 const SlideLeft = styled.div`
   animation: 2.9s ${leftSlide};
@@ -30,16 +32,18 @@ const myFadeBigGram = keyframes`${fadeOutUpBig}`;
 const FadeBigGram = styled.div`
   animation: 1.6s ${myFadeBigGram};
 `;
+const myFadeIn = keyframes`${fadeInUp}`;
+const FadeIn = styled.div`
+  animation: 2.9s ${myFadeIn};
+`;
 
 const Header = () => {
-
     return (
         <div>
             <header id="home">
                 <CustomNav />
                 <section id="intro-container">
                     <div id="intro">
-
                         <SlideLeft>Hi, I'm Phil. <span className="span-text">I design and build websites in React.
                         </span>
                             <div className="divider-two"></div>
@@ -53,18 +57,24 @@ const Header = () => {
 
                                 <Col>
                                     <FadeBigGit>
-                                        <FaGithub size={24} />
+                                        <a className="header-icons" href="https://github.com/pswartz66">
+                                            <FaGithub size={24} />
+                                        </a>
                                     </FadeBigGit>
 
                                 </Col>
                                 <Col>
                                     <FadeBigLink>
-                                        <FaLinkedinIn size={24} />
+                                        <a className="header-icons" href="https://www.linkedin.com/in/phillip-swartz-a6816199/">
+                                            <FaLinkedinIn size={24} />
+                                        </a>
                                     </FadeBigLink>
                                 </Col>
                                 <Col>
                                     <FadeBigGram>
+                                    <a className="header-icons" href="https://www.instagram.com/philswartz/?hl=en">
                                         <FaInstagram size={24} />
+                                    </a>
                                     </FadeBigGram>
                                 </Col>
                                 <Col></Col>
@@ -83,7 +93,10 @@ const Header = () => {
                         </SlideLeft>
                         <Row>
                             <Col>
-                                <FaAngleDown color={'rgb(94, 248, 114)'} size={28} />
+                                <FadeIn>
+                                    <FaAngleDown color={'rgb(94, 248, 114)'} size={28} />
+                                </FadeIn>
+
                             </Col>
                         </Row>
                     </div>
