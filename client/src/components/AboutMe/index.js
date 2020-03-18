b2a85a61a6dc75320051792a10a7d667b63e150f
Phil Swartz <pswartz.sju@gmail.com>
2020-03-17 21:22:03 -0400

    updated media query for about me section, added paragraph and contact details section, add in download resume capability

--- client/src/components/AboutMe/index.js
+++ client/src/components/AboutMe/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import { FaDownload } from 'react-icons/fa';
 import './style.css';
 import profileImg from '../../assets/img/ProfilePicture.jpg';
 
@@ -13,9 +12,29 @@ const AboutMe = (props) => {
             </div>
 
             <div className="right-pane">
-                <h2 className="aboutMeTitle">About Me</h2>
-                    <p>AboutMe section goes here</p>
+                <h2 className="Title">About Me</h2>
+                <div className="aboutMeParagraph">
+                    <p>Hey everyone! Glad you've checked out my portfolio!
+                        To give a little background, I was introduced to programming
+                        around 2012 during my time in college. After graduating I used a number
+                        of online tools like YouTube, Udemy, and free textbooks to learn about Python since
+                        I was intruged by data. Most recently I completed the Penn LPS coding bootcamp which was primarily geared
+                        towards full stack web development.
+                    </p>
                 </div>
+                <br></br>
+                <h2 className="Title">Contact Details</h2>
+                <div className="info">
+                    <ul>Phil Swartz</ul>
+                    <ul>Philadelphia, PA</ul>
+                    <ul>610-675-5479</ul>
+                    <ul>pswartz.sju@gmail.com</ul>
+                </div>
+                <button className="download-btn">
+                    <FaDownload />
+                    <span id="download-text"> Download Resume</span>
+                </button>
+            </div>
         </div>
     )
 }
