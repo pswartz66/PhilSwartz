e36d610484eb1c6ed7b7426a89ed3810cfda0bbb
Phil Swartz <pswartz.sju@gmail.com>
2020-04-20 00:01:16 -0400

    updated aboutme component and configured nodemailer

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,5 @@
-// const nodemailer = require('nodemailer');
-// const creds = require('../config/emailConfig.js');
+const nodemailer = require('nodemailer');
+const creds = require('../config/emailConfig.js');
 
 // build template email below using data from front end
 module.exports = {
@@ -9,37 +9,40 @@ module.exports = {
         console.log(req.body);
         // console.log(res.send('Response from server'));
 
+        // create a template that for email html
+        const emailBody = `<p>${req.body.message}</p>`;
+
         // create reusable transporter object using default SMTP
-        // let transporter = nodemailer.createTransport({
-        //     host: 'smtp.gmail.com',
-        //     port: 587,
-        //     secure: false, // true for 465, false for other ports
-        //     auth: {
-        //         user: creds.email,
-        //         pass: creds.key
-        //     },
-        //     tls: {
-        //         rejectUnauthorized: false
-        //     }
-        // });
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: creds.email,
+                pass: creds.key
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
 
         // /// setup email options object
-        // let mailOptions = {
-        //     from: req.body.emailAddress,
-        //     to: '<philswartzportfolio2@gmail.com>',
-        //     subject: req.body.subject ? req.body.subject : null,
-        //     text: '', // plain text body 
-        //     html: emailBody // html body see template above
-        // }
+        let mailOptions = {
+            from: req.body.emailAddress,
+            to: '<philswartzportfolio2@gmail.com>',
+            subject: req.body.subject ? req.body.subject : null,
+            text: '', // plain text body 
+            html: emailBody // html body see template above
+        }
 
         // // send mail with defined transport object
-        // transporter.sendMail(mailOptions, (error, info) => {
-        //     if (error) {
-        //         transporter.close();
-        //         return console.log(error);
-        //     }
-        //     res.json();
-        //     transporter.close();
-        // })
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                transporter.close();
+                return console.log(error);
+            }
+            res.json();
+            transporter.close();
+        })
     }
 }
